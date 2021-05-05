60eab185747008bf15094742d6304141fa092164
AkhterAfzal <afzala@legalutopia.co.uk>
2021-05-05 14:50:27 +0100

    Testing the payment for solicitors page

---
+++ js/premium-payment.js
@@ -0,0 +1,40 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+                    (function () {
+                        var stripe = Stripe('pk_test_FDPtusaaQazanMehraItyOz100isLaJZhO');
+
+                        var checkoutButton = document.getElementById('checkout-button-price_1InhRTG1VXL5zVgO4fkFGKc0');
+                        checkoutButton.addEventListener('click', function () {
+                            /*
+                             * When the customer clicks on the button, redirect
+                             * them to Checkout.
+                             */
+                            stripe.redirectToCheckout({
+                                lineItems: [{price: 'price_1InhRTG1VXL5zVgO4fkFGKc0', quantity: 1}],
+                                mode: 'payment',
+                                /*
+                                 * Do not rely on the redirect to the successUrl for fulfilling
+                                 * purchases, customers may not always reach the success_url after
+                                 * a successful payment.
+                                 * Instead use one of the strategies described in
+                                 * https://stripe.com/docs/payments/checkout/fulfill-orders
+                                 */
+                                successUrl: 'https://legalutopia.services/success',
+                                cancelUrl: 'https://legalutopia.services/canceled',
+                            })
+                                    .then(function (result) {
+                                        if (result.error) {
+                                            /*
+                                             * If `redirectToCheckout` fails due to a browser or network
+                                             * error, display the localized error message to your customer.
+                                             */
+                                            var displayError = document.getElementById('error-message');
+                                            displayError.textContent = result.error.message;
+                                        }
+                                    });
+                        });
+                    })();
